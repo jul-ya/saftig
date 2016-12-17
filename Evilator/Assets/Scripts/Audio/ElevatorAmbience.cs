37e3a654b2f202daab6219d827aefcd8f1566a3b
Ju Lia <jul.ya@gmx.at>
2016-12-17 22:15:58 +0100

    moar audio

---
+++ Evilator/Assets/Scripts/Audio/ElevatorAmbience.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorAmbience : MonoBehaviour {
+
+    public AudioClip elevatorStart;
+    public AudioClip[] elevatorLoops;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
