8b8b105bbc4adb7dc560e63b90f7836b00bac2bb
Andreas Pointner <pointner.andy@gmail.com>
2016-12-17 15:49:27 +0100

    added new state

---
+++ Evilator/Assets/Scripts/StateMachine/States/StunState.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using InControl;
+using UnityEngine;
+
+public class StunState : State {
+    public StunState(float prepareTime, float performTime, float cooldownTime) : base(prepareTime, performTime, cooldownTime, StateID.StunState)
+    {
+    }
+
+    public override void Reason(GameObject player, InputDevice inputDevice)
+    {
+        player.GetComponent<Player>().SetTransition(StateID.IdleState);
+    }
+
+    protected override void Cooldown(GameObject player, InputDevice inputDevice)
+    {
+        Debug.Log("stun cooldown State");
+    }
+
+    protected override void PerformAction(GameObject player, InputDevice inputDevice)
+    {
+        Debug.Log("Crouch State");
+    }
+
+    protected override void Prepare(GameObject player, InputDevice inputDevice)
+    {
+        Debug.Log("stun prepare State");
+    }
+}
