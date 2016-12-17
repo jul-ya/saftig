f97cc1b15c89ef8e81cd1ce0996a65c8998ba08f
Philipp Stadler <hello@phstadler.com>
2016-12-17 14:30:04 +0100

    Got it kinda working

--- Evilator/Assets/Scripts/Physics.cs
+++ Evilator/Assets/Scripts/Physics.cs
@@ -4,32 +4,42 @@ using UnityEngine;
 
 public class Physics : MonoBehaviour {
 
+	public float jumpStartSpeed = 2.0f;
 	public float walkSpeed = 1.0f;
+	public float gravity = 9.81f;
 
-	private Vector3 veloctiy;
+	private Rigidbody body;
+
+	void Start() {
+		body = GetComponent<Rigidbody> ();
+	}
 
 	/**
 	 *  Called every frame with direction between -1 and 1, standing still is exactly 0.0.
 	 */
 	public void PerformMove(float direction) {
-		Vector3 pos = transform.position;
-
-		pos.x += direction * walkSpeed * Time.deltaTime;
-
-		transform.position =  pos;
+		Vector3 velocity = body.velocity;
+		velocity.x = direction * walkSpeed * Time.deltaTime;
+		body.velocity = velocity;
 	}
 
 	public void PerformJump() {
+		Vector3 velocity = body.velocity;
+		velocity.y = jumpStartSpeed;
+		body.velocity = velocity;
+	}
+
+	void ApplyGravity() {
 
 	}
 
-	// Use this for initialization
-	void Start () {
+	void Move() {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		ApplyGravity();
+		Move();
 	}
 }
