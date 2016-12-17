2398277cea5a7a119bb8b78fba1d0928020a7bfe
Philipp Stadler <hello@phstadler.com>
2016-12-17 21:51:47 +0100

    Player now has phone in hand, not bad.

---
+++ Evilator/Assets/Scripts/PhoneHand.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhoneHand : MonoBehaviour {
+
+	public Transform hand;
+	public Transform phonePrefab;
+
+	private GameObject phone;
+
+	public bool hasPhone {
+		set {
+			phone.SetActive(value);
+			SendMessageUpwards("setActivePlayer", GetComponent<Player> ());
+		}
+
+		get {
+			return phone.activeSelf;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		phone = GameObject.Instantiate(phonePrefab, hand).gameObject;
+		phone.transform.localPosition = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
