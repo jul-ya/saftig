b355d6cb91e47f0416bd83f149e281a96a5e9b4f
Philipp Stadler <hello@phstadler.com>
2016-12-17 14:14:02 +0100

    Added physics but they don’t work yet, sorry

--- Evilator/Assets/Scripts/Controls.cs
+++ Evilator/Assets/Scripts/Controls.cs
@@ -5,15 +5,25 @@ using InControl;
 
 public class Controls : MonoBehaviour {
 
+	public InputControlType moveControl = InputControlType.LeftStickX;
+	public InputControlType jumpControl = InputControlType.Action1;
+
 	public InputDevice dev;
+
+	private Physics physics;
 
 	// Use this for initialization
 	void Start () {
-		
+		physics = GetComponent<Physics> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		float moveDirection = dev.GetControl(moveControl).Value;
+		physics.PerformMove(moveDirection);
+
+		if(dev.GetControl(jumpControl).WasPressed) {
+			physics.PerformJump();
+		}
 	}
 }
