2612e6a84bc64840d91b0702bfb7732ab8aa7461
Philipp Stadler <hello@phstadler.com>
2016-12-17 17:21:34 +0100

    Added range and correct orientation of players when changing movement direction

---
+++ Evilator/Assets/Scripts/Range.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Range : MonoBehaviour {
+
+	public HashSet<Transform> playersInRange = new HashSet<Transform>();
+
+	void OnTriggerEnter(Collider other) {
+		if(other.tag == "Player") {
+			playersInRange.Add(other.transform);
+			print("In range: " + playersInRange.Count);
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		if(other.tag == "Player") {
+			playersInRange.Remove(other.transform);
+		}
+	}
+}
