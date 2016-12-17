3036613c2814b9574878a72f74547cb353b307d9
Philipp Stadler <hello@phstadler.com>
2016-12-17 18:15:15 +0100

    Added action activations

--- Evilator/Assets/Scripts/Controls.cs
+++ Evilator/Assets/Scripts/Controls.cs
@@ -7,14 +7,18 @@ public class Controls : MonoBehaviour {
 
 	public InputControlType moveControl = InputControlType.LeftStickX;
 	public InputControlType jumpControl = InputControlType.Action1;
+	public InputControlType attackControl = InputControlType.Action3;
+	public InputControlType blockControl = InputControlType.Action4;
 
 	public InputDevice dev;
 
 	private Physics physics;
+	private Player player;
 
 	// Use this for initialization
 	void Start () {
 		physics = GetComponent<Physics> ();
+		player = GetComponent<Player> ();
 	}
 	
 	// Update is called once per frame
@@ -25,5 +29,11 @@ public class Controls : MonoBehaviour {
 		if(dev.GetControl(jumpControl).WasPressed) {
 			physics.PerformJump();
 		}
+
+		if(dev.GetControl(blockControl).WasPressed) {
+			player.Block();
+		} else if(dev.GetControl(attackControl).WasPressed) {
+			player.Attack();
+		} 
 	}
 }
--- Evilator/Assets/Scripts/StateMachine/Player.cs
+++ Evilator/Assets/Scripts/StateMachine/Player.cs
@@ -26,14 +26,28 @@ public class Player : MonoBehaviour
         machine.CurrentState.Act(gameObject, inputDevice);
     }
 
+	public void Attack()
+	{
+		if(machine.CurrentStateID == StateID.IdleState) {
+			machine.PerformTransition(StateID.AttackState);
+		}
+	}
+
+	public void Block()
+	{
+		if(machine.CurrentStateID == StateID.IdleState) {
+			machine.PerformTransition(StateID.BlockState);
+		}
+	}
+
     private void SetupStateMachine()
     {
         IdleState idle = new IdleState(0.0f, 3.0f, 0.0f);
-
         AttackState attack = new AttackState(2.0f, 5.0f, 4.2f);
 
         machine = new StateMachine();
         machine.AddState(idle);
         machine.AddState(attack);
+		machine.PerformTransition(StateID.IdleState);
     }
 }
