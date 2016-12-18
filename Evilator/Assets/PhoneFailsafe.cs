d0e984c889708a81bf085bf8b92d2f8c1ce39a15
Philipp Stadler <hello@phstadler.com>
2016-12-18 16:52:08 +0100

    Added phone failsafe

---
+++ Evilator/Assets/PhoneFailsafe.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhoneFailsafe : MonoBehaviour {
+
+	public Transform phoneResetHeight;
+
+	private Vector3 initialPosition;
+	private Rigidbody body;
+
+	// Use this for initialization
+	void Start () {
+		initialPosition = transform.position;
+		body = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(transform.position.y < phoneResetHeight.position.y) {
+			transform.position = initialPosition;
+			body.velocity = Vector3.zero;
+		}
+	}
+}
