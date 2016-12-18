afa722ae8c479393c1221f6ce80160c5791bd20c
urstruktur <s1310238052@students.fh-hagenberg.at>
2016-12-18 10:49:50 +0100

    added panel handler

---
+++ Evilator/Assets/Scripts/PanelHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelHandler : MonoBehaviour {
+
+    public GameObject[] panel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
